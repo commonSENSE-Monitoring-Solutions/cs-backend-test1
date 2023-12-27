ce399291699abdcb1d4e5733774d941350f884a4
foxstudent106092 <nik.datascience@gmail.com>
2023-12-27 11:57:23 +0000

    Add main func that runs migration

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"cs-backend-test1/config"
+	"cs-backend-test1/internal/converter"
+	"cs-backend-test1/internal/migrator"
+	"cs-backend-test1/internal/model"
+	"cs-backend-test1/internal/storage"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
-	fmt.Println("Hello, potential employee!")
+	cfg, err := config.LoadAndParse()
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	zerolog.SetGlobalLevel(zerolog.Level(cfg.Logger.LogLevel))
+
+	userConverter := new(converter.UserConverter)
+	m := migrator.NewUserMigrator(userConverter)
+
+	sourceStorage := storage.NewDatabase("cs_old", &model.User{})
+	if err = sourceStorage.OpenWithConfig(&cfg.Db); err != nil {
+		log.Fatal().Err(err).Send()
+	}
+	sourceStorage.SetMigrator(m)
+
+	destStorage := storage.NewDatabase("cs_new", &model.UserAccount{})
+	if err = destStorage.OpenWithConfig(&cfg.Db); err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	var users []model.User
+	err = sourceStorage.MigrateDataBatches(destStorage, &users, 5)
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
 }
